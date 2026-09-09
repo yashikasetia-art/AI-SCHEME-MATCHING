3df8eb81d7e4542f44e62c8b43c18524c5b54370
yashikasetia-art <yashikasetia538@gmail.com>
2026-09-09 23:19:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,12 @@ app.get('/api/gov-data', async (req, res) => {
     }
 });
 
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 NSFDC Backend Proxy running at http://localhost:${PORT}`);
 });
